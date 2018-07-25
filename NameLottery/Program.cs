782cd76690b359ae18042721de27a6296a17160e
Adriana Disano <adrianadisano@gmail.com>
2018-07-25 06:08:38 -0500

    Add project files.

---
+++ NameLottery/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NameLottery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
