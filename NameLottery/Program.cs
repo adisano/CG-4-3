1fcd8da9a240ec097edc24ff40b99ae154edce32
Adriana Disano <adrianadisano@gmail.com>
2018-08-06 16:21:20 -0500

    -Fixed a bug where names could be entered after a winner was chosen.

--- NameLottery/Program.cs
+++ NameLottery/Program.cs
@@ -6,39 +6,49 @@ namespace NameLottery
     {
         static void Main(string[] args)
         {
+            //prompt user for input
             Console.WriteLine("This program can select a winner randomly from out of up to 30 names.");
             Console.WriteLine("Please begin entering names to choose from. Press ENTER after each name or once you are finished.");
 
+            //initialize an array for names
             string[] namesList = new string[30];
 
+            //initialize a random seed
+            //this is for picking a winner
             Random namesRandomizer = new Random();
 
-            //accept 30 names
+            //iterate through this loop up to 30 times
+            //this is for adding names to the raffle
             for (int namesLimit = 0; namesLimit < 30; namesLimit++)
             {
 
-                //add names to array
+                //prompt user for names and add them to the array
                 string namesNew = Console.ReadLine();
                 namesList[namesLimit] = namesNew;
 
-                //choose and display random name, all entries
+                //execute if the user presses enter without typing any input
                 if (namesNew == "")
                 {
+                    //assign value to an integer according to random seed
                     int namesWinner = namesRandomizer.Next(namesLimit);
+                    //display the winner and all entrants
                     Console.WriteLine("The winning name is " + namesList[namesWinner] + "!");
                     Console.WriteLine("Thank you to everyone who entered:");
                     foreach (string namesLosers in namesList)
                     {
                         Console.WriteLine(namesLosers);
                     }
+                    break;
                 }
                 else
                 {
+                    //displays name entered and gives user further instructions
                     if (namesLimit < 29)
                     {
                         Console.WriteLine("Name " + (namesLimit + 1) + " is " + namesNew + ". Please continue entering names or press ENTER to finish.");
 
                     }
+                    //displays name entered, tells the user this was the last name they can enter
                     else
                     {
                         Console.WriteLine("Name " + (namesLimit + 1) + " is " + namesNew + ". The list of names is now full.");
@@ -47,6 +57,7 @@ namespace NameLottery
 
                 
             }
+            //await input before closing program
             Console.ReadLine();
 
         }
